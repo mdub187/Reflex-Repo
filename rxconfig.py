3544d3ac9c08f94095911977890f54f2c17bd630
mdub187 <maweeks.91@gmail.com>
2025-10-01 12:35:17 -0600

    1st push

---
+++ rxconfig.py
@@ -0,0 +1,17 @@
+import reflex as rx
+
+
+class Config(rx.Config):
+    app_name = "lmrex"
+    styles = [
+        "styles.css",
+        "./styles/styles.css",
+    ]  # make sure you actually have a styles.css file
+    plugins = [
+        rx.plugins.SitemapPlugin(),
+        rx.plugins.TailwindV4Plugin(),
+    ]
+
+
+print(Config(app_name="lmrex"))
+config = Config(app_name="lmrex")

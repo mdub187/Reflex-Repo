f8e74f86fc0139af5cdeaae94f3fef0917a5c984
Marc Dub <126857323+mdub187@users.noreply.github.com>
2025-10-16 12:11:06 -0600

    Revert "New"

--- README.md
+++ README.md
@@ -4,21 +4,13 @@
 # Desription
   Template web-application project utitilizing reflex framework, built in python.
 # Install
-  1. Create virtual environment:
-    """ python3 -m venv .venv """
-  2. Activate Said Environment:
-    """ source .venv/bin/activate """
-  3. Install dependencies:
-    """ pip install -r requirements.txt """
-  4. run reflex:
-    """ reflex run """
+  undefined
 # Usage
-  Extensive portfolio/ e-commerce web app using only python and reflex libraries.
+  Simple web app
 # Contribution
-  Working on getting the reg ex for the auth login, and the redirect.
   any
 # Testing
-  TBD
+  n/a
 # Liscense
   undefined
 # Github Username
--- lmrex/ui/login.py
+++ lmrex/ui/login.py
@@ -8,27 +8,24 @@ def user_login() -> rx.Component:
     print("Debug: user_login component is being rendered")
     return rx.box(
         rx.vstack(
-            rx.alert_dialog.root(
-                rx.alert_dialog.content(
-                rx.alert_dialog.trigger(
-                    rx.box(
-                        rx.hstack(
-                            rx.heading("Debug Modal"),
-                            size="3",
-                            # rx.box("This is a simple modal for debugging."),
-                            justify="center"
-                        ),
-                        rx.button("Close", on_click=State.toggle_modal),
-                        rx.alert_dialog.cancel(
-                            rx.button(
-                                "Cancel",
-                                variant="soft",
-                            color_scheme="gray",
+            rx.alert_dialog.trigger(
+                rx.box(
+                    rx.hstack(
+                    rx.heading("Debug Modal"),
+                    size="3",
+                    # rx.box("This is a simple modal for debugging."),
+                        justify="center"
+                    ),
+                    rx.button("Close", on_click=State.toggle_modal),
+            rx.alert_dialog.cancel(
+                    rx.button(
+                        "Cancel",
+                        variant="soft",
+                        color_scheme="gray",
                         ),
                     ),
                 # is_open=True,  # Force modal to always render for debugging
                 ), # Toggle modal visibility on close action
-            ),
             ),
             spacing="5",
             justify="center",
@@ -41,4 +38,3 @@ def user_login() -> rx.Component:
             color_mode(),
         ),
     )
-)

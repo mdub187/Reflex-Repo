1e8174bf22c9f88098c3230fd7ae4ff4d0289db8
Marc Dub <126857323+mdub187@users.noreply.github.com>
2025-11-14 20:59:29 -0700

    Update lmrex.py

--- lmrex/lmrex.py
+++ lmrex/lmrex.py
@@ -5,18 +5,10 @@
 from lmrex.routes.routes import add_routes, app
 from lmrex.ui.responsive_utils import apply_responsive_styles
 
-# path = "/Users/mdub/Documents/Dev End/User Python/Reflex_pylot/lmrex"
-# print(path)
-# sys.path.append(path)
 # Apply responsive styles
 responsive_styles = apply_responsive_styles()
 
-
-# sys.path.append(".")
-# sys.path.append("lmrex")
-
-
-# print(sys.path)
+# init app
 app()
 add_routes(app)
 
